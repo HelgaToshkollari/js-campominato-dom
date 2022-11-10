4c62430d3c5ed016bb678872874e12a0729a5fa7
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-10 16:54:59 +0100

    adds bombs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 const btnGenerate = document.querySelector(".btn-play");
 const levelList = document.querySelector( "[name='difficulty']");
-
+let bombs;
 
 
 btnGenerate.addEventListener("click", function(){
-    
+
    const level = levelList.value;
+
+   bombs = generateBombs(+level);
 
    generateSquares(+level);
 
@@ -14,6 +16,7 @@ btnGenerate.addEventListener("click", function(){
 
 function generateSquares ( squaresNbr ){
     const squareContainer = document.querySelector(".container");
+
     squareContainer.innerHTML = "";
 
     for (let i = + 1; i <= squaresNbr; i++ ){
@@ -37,8 +40,33 @@ function generateSquares ( squaresNbr ){
 
 function onSquareClick (){
     const squaresNbr = +this.dataset.squaresNbr;
-    this.classList.toggle("active");
-    console.log(squaresNbr);
 
-   
+    if(bombs.includes(squaresNbr)){
+        alert("game over");
+        this.classList.add("active", "bomb");
+    } else {
+        this.classList.toggle("active");
+    }
+
+
+}
+
+function generateRandomNbr (min, max){
+    return Math.floor(Math.random()* (max - min +1 )) + min;
+}
+
+function generateBombs (squaresNbr){
+    const bombsArray = [];
+    console.log(bombsArray)
+
+    while (bombsArray.length < 16 ) {
+        const nbr = generateRandomNbr ( 1, squaresNbr);
+
+        if (!bombsArray.includes(nbr)){
+            bombsArray.push(nbr);
+        }
+    }
+
+    return bombsArray;
+
 }
