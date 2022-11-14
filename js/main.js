8cf8875033f6df6603512ae7c962fdf5c3872b72
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-14 16:13:23 +0100

    adds changes

--- js/main.js
+++ js/main.js
@@ -4,15 +4,16 @@ const levelList = document.querySelector( "[name='difficulty']");
 let bombs;
 
 
-btnGenerate.addEventListener("click", function(){
 
-   const level = levelList.value;
 
-   bombs = generateBombs(+level);
+btnGenerate.addEventListener("click", ()=>{
 
-   generateSquares(+level);
+    const level = levelList.value;
+    bombs = generateBombs(+level);
+    generateSquares(+level);}
 
-} );
+)
+
 
 function generateSquares ( squaresNbr ){
     const squareContainer = document.querySelector(".container");
@@ -25,6 +26,7 @@ function generateSquares ( squaresNbr ){
         const rowNbr = Math.sqrt(squaresNbr);
 
         newSquare.classList.add("squares");
+        
         newSquare.style.flexBasis = 100 / rowNbr + "%";
 
         newSquare.textContent = i ;
@@ -42,8 +44,26 @@ function onSquareClick (){
     const squaresNbr = +this.dataset.squaresNbr;
 
     if(bombs.includes(squaresNbr)){
-        alert("game over");
-        this.classList.add("active", "bomb");
+        
+        this.classList.add("active", "bomb"); 
+
+
+        bombs.forEach(element => {
+            
+            document.getElementsByClassName('squares')[element - 1].classList.add("active", "bomb");
+
+
+        });
+        
+        setTimeout(()=>{    
+            const level = levelList.value;
+            bombs = generateBombs(+level);
+            generateSquares(+level); 
+            alert('GAME OVER!!');
+
+        }, 200)
+        
+
     } else {
         this.classList.toggle("active");
     }
@@ -55,6 +75,8 @@ function generateRandomNbr (min, max){
     return Math.floor(Math.random()* (max - min +1 )) + min;
 }
 
+
+
 function generateBombs (squaresNbr){
     let bombsArray = [];
     console.log(bombsArray) 
@@ -64,6 +86,7 @@ function generateBombs (squaresNbr){
         const nbr = generateRandomNbr ( 1, squaresNbr);
         if (!bombsArray.includes(nbr)){
             bombsArray.push(nbr);
+            
             
         }
     }
